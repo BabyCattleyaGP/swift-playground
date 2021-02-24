ac7a43f79121e8a0f03bee35ea707b196e87b84b
BabyCattleyaGP <bcattleya@gmail.com>
2021-02-24 10:43:27 +0700

    minimal code to passed test

--- tdd.playground/Contents.swift
+++ tdd.playground/Contents.swift
@@ -1,14 +1,12 @@
 import XCTest
 
 func addition(num1 a:Int, num2 b:Int) -> Int {
-    return 0
+    return a+b
 }
 
 class ClassTest: XCTestCase {
     func testSum() {
-        let firstNum = 2
-        let secondNum = 3
-        XCTAssertEqual(addition(num1: firstNum, num2: secondNum), 5)
+        XCTAssertEqual(addition(num1: 2, num2: 3), 5)
     }
 }
 
